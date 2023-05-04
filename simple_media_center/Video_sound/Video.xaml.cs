8cbdb4b4e8ccdcd45406ddc1f4fc75fbcc424208
Mateo Čić <mcic20@foi.hr>
2023-05-04 21:31:22 +0200

    #3 Videos are shown on screen.

--- simple_media_center/Video_sound/Video.xaml.cs
+++ simple_media_center/Video_sound/Video.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Video_sound
 {
@@ -23,6 +13,21 @@ namespace Video_sound
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Videos|*.mp4; *.mkv; *.webm; *.avi";
+            ofd.Multiselect = false;
+            ofd.ShowDialog();
+            try {
+                elementVideo.Source = new Uri(ofd.FileName);
+            }
+            catch
+            {
+
+            }
         }
     }
 }
